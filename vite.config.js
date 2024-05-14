3b6e11690d3d8c997a514dbb668267454105e4e1
med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2024-05-14 14:37:16 +0200

    mise a jour des dependance et 1ere version prete

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Configurer Vite pour utiliser le dossier 'build' comme sortie
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: "/src" }],
-  },
+  build: {
+    outDir: 'build',  // Définir le répertoire de sortie pour les builds de production
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500  // Limite pour les avertissements sur la taille des chunks
+  }
 });
