8b089d0649528bfdc21528f3b24834642d987ece
med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2024-05-14 15:11:06 +0200

    mise a jour des dependance et 1ere version prete

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,16 @@ export default defineConfig({
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
+  build: {
+    outDir: 'build',  // Spécifiez 'build' comme répertoire de sortie
+    rollupOptions: {
+      output: {
+        // Ajoutez des options de chunking pour optimiser la taille des bundles
+        manualChunks: {
+          vendor: ['react', 'react-dom']  // Regroupe les dépendances communes
+        }
+      }
+    },
+    chunkSizeWarningLimit: 500,  // Augmentez la limite pour réduire les avertissements de taille de chunk
+  }
 });
