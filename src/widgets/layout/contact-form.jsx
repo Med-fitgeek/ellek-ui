4ad0bdeae71906d34e045d8b183e0cbddf95e533
med-fitgeek <ndongomedoune.ndiaye@gmail.com>
2024-05-15 16:14:41 +0200

    mise a jour des dependance et 1ere version prete

--- src/widgets/layout/contact-form.jsx
+++ src/widgets/layout/contact-form.jsx
@@ -3,13 +3,13 @@ import { Input, Textarea, Button } from "@material-tailwind/react";
 
 const ContactForm = () => {
   return (
-    <form className="contact-form mx-auto mt-12 max-w-3xl text-center">
-      <div className="mb-8 flex gap-8">
-        <Input variant="standard" size="lg" label="Nom complet" />
-        <Input variant="standard" size="lg" label="Adresse e-mail" />
+    <form className="mx-auto mt-12 max-w-3xl text-center p-2.5 md:p-0">
+      <div className="flex flex-col gap-8 md:flex-row md:gap-8 mb-8">
+        <Input variant="standard" size="lg" label="Nom complet" className="w-full" />
+        <Input variant="standard" size="lg" label="Adresse e-mail" className="w-full" />
       </div>
-      <Textarea variant="standard" size="lg" label="Message" rows={8} />
-      <Button variant="gradient" size="lg" className="mt-8">
+      <Textarea variant="standard" size="lg" label="Message" rows={8} className="w-full" />
+      <Button variant="gradient" size="lg" className="mt-8 w-full md:w-auto">
         Envoyer un message
       </Button>
     </form>
